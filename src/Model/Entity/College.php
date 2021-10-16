2a6675baffbdf1b821757900c6c82b02f03bb734
JensFillee <r0800028@student.thomasmore.be>
2021-10-16 14:23:15 +0200

    models & entities aangemaakt

---
+++ src/Model/Entity/College.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+class College extends Entity
+{
+    // in $_accessible: define all columns that need to be inserted into table
+    protected $_accessible = [];
+}
