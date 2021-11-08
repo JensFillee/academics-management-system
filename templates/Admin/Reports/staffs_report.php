3d60c687e0ece64445583d604bd244dfa3f33447
JensFillee <r0800028@student.thomasmore.be>
2021-11-08 13:10:57 +0100

    report templates (colleges, staff & students) with export features

--- templates/Admin/Reports/staffs_report.php
+++ templates/Admin/Reports/staffs_report.php
@@ -1,5 +1,111 @@
 <?php
-if(!empty($title)) {
+if (!empty($title)) {
     $this->assign("title", $title);
 }
+
+// DataTables
+$this->html->css([
+    "/plugins/datatables-bs4/css/dataTables.bootstrap4.css",
+    "buttons.dataTables.min.css" /* no '/' needed because it is in webroot/css directory */
+], ["block" => "topStyleLinks"])
+?>
+
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h3>Staff Report</h3>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">Staff Report</li>
+                </ol>
+            </div>
+        </div>
+    </div><!-- /.container-fluid -->
+</section>
+
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <!-- left column -->
+            <div class="col-md-12">
+                <!-- general form elements disabled -->
+                <div class="card card-secondary">
+                    <div class="card-header">
+                        <h3 class="card-title">Staff Report</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <table id="tbl-staff" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>#ID</th>
+                                    <th>Staff Info</th>
+                                    <th>College/Branch</th>
+                                    <th>Designation</th>
+                                    <th>Gender</th>
+                                    <th>Profile Image</th>
+                                    <th>Status</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th>#ID</th>
+                                    <th>Staff Info</th>
+                                    <th>College/Branch</th>
+                                    <th>Designation</th>
+                                    <th>Gender</th>
+                                    <th>Profile Image</th>
+                                    <th>Status</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+
+            </div>
+        </div>
+        <!-- /.row -->
+    </div><!-- /.container-fluid -->
+</section>
+
+<!-- DataTables -->
+<?=
+$this->Html->script([
+    "/plugins/datatables/jquery.dataTables.js",
+    "/plugins/datatables-bs4/js/dataTables.bootstrap4.js",
+    "dataTables.buttons.min.js", /* no '/' needed because it is in webroot/js directory */
+    "jszip.min.js",
+    "pdfmake.min.js",
+    "vfs_fonts.js",
+    "buttons.html5.min.js"
+], ["block" => "bottomScriptLinks"]);
+?>
+
+<?php
+// open tag: <script>
+$this->Html->scriptStart(["block" => true]);
+// content of script tag (for exports)
+echo "$('#tbl-staff').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'copyHtml5',
+            'excelHtml5',
+            'csvHtml5',
+            'pdfHtml5'
+        ]
+    } );";
+// close tag: </script>
+$this->Html->scriptEnd();
 ?>
--- templates/Admin/Reports/students_report.php
+++ templates/Admin/Reports/students_report.php
@@ -1,5 +1,109 @@
 <?php
-if(!empty($title)) {
+if (!empty($title)) {
     $this->assign("title", $title);
 }
+
+// DataTables
+$this->html->css([
+    "/plugins/datatables-bs4/css/dataTables.bootstrap4.css",
+    "buttons.dataTables.min.css" /* no '/' needed because it is in webroot/css directory */
+], ["block" => "topStyleLinks"])
+?>
+
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h3>Students Report</h3>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">Students Report</li>
+                </ol>
+            </div>
+        </div>
+    </div><!-- /.container-fluid -->
+</section>
+
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <!-- left column -->
+            <div class="col-md-12">
+                <!-- general form elements disabled -->
+                <div class="card card-secondary">
+                    <div class="card-header">
+                        <h3 class="card-title">Students Report</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <table id="tbl-students" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>#ID</th>
+                                    <th>Student Info</th>
+                                    <th>College/Branch</th>
+                                    <th>Gender</th>
+                                    <th>Profile Image</th>
+                                    <th>Status</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th>#ID</th>
+                                    <th>Student Info</th>
+                                    <th>College/Branch</th>
+                                    <th>Gender</th>
+                                    <th>Profile Image</th>
+                                    <th>Status</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+
+            </div>
+        </div>
+        <!-- /.row -->
+    </div><!-- /.container-fluid -->
+</section>
+
+<!-- DataTables -->
+<?=
+$this->Html->script([
+    "/plugins/datatables/jquery.dataTables.js",
+    "/plugins/datatables-bs4/js/dataTables.bootstrap4.js",
+    "dataTables.buttons.min.js", /* no '/' needed because it is in webroot/js directory */
+    "jszip.min.js",
+    "pdfmake.min.js",
+    "vfs_fonts.js",
+    "buttons.html5.min.js"
+], ["block" => "bottomScriptLinks"]);
+?>
+
+<?php
+// open tag: <script>
+$this->Html->scriptStart(["block" => true]);
+// content of script tag (for exports)
+echo "$('#tbl-students').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'copyHtml5',
+            'excelHtml5',
+            'csvHtml5',
+            'pdfHtml5'
+        ]
+    } );";
+// close tag: </script>
+$this->Html->scriptEnd();
 ?>
