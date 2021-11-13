eef308bcd4e9742b475acfd79d34f0a4bd4c7366
JensFillee <r0800028@student.thomasmore.be>
2021-11-13 10:42:00 +0100

    dashboard counts (colleges, students & staff)

--- src/Controller/Admin/DashboardsController.php
+++ src/Controller/Admin/DashboardsController.php
@@ -11,9 +11,45 @@ class DashboardsController extends AppController
         parent::initialize();
         // Use templates/layout/admin.php as default layout of this controller
         $this->viewBuilder()->setLayout("admin");
+
+        // Load models
+        $this->loadModel("Colleges");
+        $this->loadModel("Students");
+        $this->loadModel("Staffs");
     }
 
-    public function index() {
+    public function index()
+    {
+        // Colleges Count
+        $collegeQuery = $this->Colleges->find();
+
+        $collegesData = $collegeQuery->select([
+            "total_data" => $collegeQuery->func()->count("*") /* make a new attribute with value = count of all college-records */
+        ])->first();
+
+        $collegesCount = $collegesData->total_data; /* $collegesCount = the value of total_data */
+
+        // Students Count
+        $studentQuery = $this->Students->find();
+
+        $studentsData = $studentQuery->select([
+            "total_data" => $studentQuery->func()->count("*")
+        ])->first();
+
+        $studentsCount = $studentsData->total_data;
+
+        // Staff Count
+        $staffQuery = $this->Staffs->find();
+
+        $staffData = $staffQuery->select([
+            "total_data" => $staffQuery->func()->count("*")
+        ])->first();
+
+        $staffCount = $staffData->total_data;
+
+        // Use variables in view
+        $this->set(compact("collegesCount", "studentsCount", "staffCount"));
+
         $this->set("title", "Admin Dashboard | Academics Management");
     }
 }
